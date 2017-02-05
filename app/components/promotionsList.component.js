b79c0e33606e5f613718b8778557b3e8aa949c4e
Robert Obreczarek <robert@obreczarek.com>
2017-02-05 18:23:08 +0100

    Fixed promotions cart
    This commit resolves the issue of promoted items not
    properly adding to the cart. This code is a duplication
    of how things are handled for regular products. A great
    opportunity for a refactor.

--- app/components/promotionsList.component.js
+++ app/components/promotionsList.component.js
@@ -1,9 +1,23 @@
-const template = `<product-item ng-repeat="product in $ctrl.products" item="product"></product-item>`
+const template = `
+  <product-item
+    ng-repeat="product in $ctrl.products"
+    item="product"
+    on-add-to-cart="$ctrl.addToCart(product, amount)">
+  >
+  </product-item>`
+
+class PromotionsListController {
+  addToCart(product, amount) {
+    this.requestAddToCart({ product: product, amount: amount })
+  }
+}
 
 export const name = 'promotionsList'
 export const properties = {
-  template,
   bindings: {
-    products: '<items'
-  }
+    products: '<items',
+    requestAddToCart: '&onAddToCart'
+  },
+  controller: PromotionsListController,
+  template
 }
