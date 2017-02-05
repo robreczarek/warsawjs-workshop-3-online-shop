ab0ab8efb692bda4ec8c48f5fe860ed1aab77c98
Robert Obreczarek <robert@obreczarek.com>
2017-02-05 12:31:44 +0100

    Logo handling and file update
    The logo is now a stylish cat. Also, the size property
    now gives sizes based on the parameter value.

--- app/components/pageHeader.component.js
+++ app/components/pageHeader.component.js
@@ -2,7 +2,7 @@ export const name = 'pageHeader'
 export const properties = {
   template: `
     <div>
-      <shop-logo size="medium"></shop-logo>
+      <shop-logo size="medium"></shop-logo> Meowza Online Shop
     </div>
   `
 }
--- app/components/shopLogo.component.js
+++ app/components/shopLogo.component.js
@@ -1,9 +1,22 @@
+class ShopLogoController {
+  constructor() {
+    this.source = './images/logo.png'
+    //this.width = 'auto'
+    this.sizes = new Map([['medium', '100px'], ['small', '30px']])
+  }
+
+  $onInit() {
+    this.width = this.sizes.get(this.size) || '60px'
+  }
+}
+
 export const name = 'shopLogo'
 export const properties = {
-  template: `
-    <div>
-      <img src="./images/warsawjs-logo.png" border="0" />
-    </div>
-  `
+  bindings: {
+    size: '@'
+  },
+  controller: ShopLogoController,
+  source: './images/logo.png',
+  template: `<image ng-src="{{ $ctrl.source }}" width="{{ $ctrl.width }}">`
 }
 
